c59e40e0ec2ea62d2fcb0cc797fb892c71344211
Matteo <donnini.mat@gmail.com>
2022-11-29 14:31:58 +0100

    Primo commit

--- app/src/main/java/com/example/androidgame/Player.java
+++ app/src/main/java/com/example/androidgame/Player.java
@@ -11,15 +11,14 @@ import android.graphics.Rect;
 public class Player implements GameObject{
 
     private Rect rectangle;
-    private int color;
     private Animation idle;
     private Animation walk_left;
     private Animation walk_right;
+    private Animation up;
     private AnimationManager anim;
 
     public Player(Rect r, int c){
         this.rectangle = r;
-        this.color = c;
 
         BitmapFactory bf = new BitmapFactory();
         Bitmap idleImg = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.idle);
@@ -27,6 +26,10 @@ public class Player implements GameObject{
         Bitmap walk2 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.walkleft2);
         Bitmap walk3 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.walkleft3);
         Bitmap walk4 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.walkleft4);
+        Bitmap up1 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.up1);
+        Bitmap up2 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.up2);
+        Bitmap up3 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.up3);
+        Bitmap up4 = bf.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.up4);
 
         idle = new Animation(new Bitmap[]{idleImg}, 2);
 
@@ -36,13 +39,15 @@ public class Player implements GameObject{
         //RIGHT
         Matrix m = new Matrix();
         m.preScale(-1,1);
-        walk1 = Bitmap.createBitmap(walk1,0,0,walk1.getWidth(),walk1.getHeight(),m,false);
-        walk2 = Bitmap.createBitmap(walk1,0,0,walk2.getWidth(),walk2.getHeight(),m,false);
-        walk3 = Bitmap.createBitmap(walk1,0,0,walk3.getWidth(),walk3.getHeight(),m,false);
-        walk4 = Bitmap.createBitmap(walk1,0,0,walk4.getWidth(),walk4.getHeight(),m,false);
+        Bitmap walk_r1 = Bitmap.createBitmap(walk1,0,0,walk1.getWidth(),walk1.getHeight(),m,false);
+        Bitmap walk_r2 = Bitmap.createBitmap(walk2,0,0,walk2.getWidth(),walk2.getHeight(),m,false);
+        Bitmap walk_r3 = Bitmap.createBitmap(walk3,0,0,walk3.getWidth(),walk3.getHeight(),m,false);
+        Bitmap walk_r4 = Bitmap.createBitmap(walk4,0,0,walk4.getWidth(),walk4.getHeight(),m,false);
 
-        walk_right = new Animation(new Bitmap[]{walk1, walk2, walk3,walk4}, 0.25f);
-        anim = new AnimationManager(new Animation[]{idle, walk_right, walk_left});
+        walk_right = new Animation(new Bitmap[]{walk_r1, walk_r2, walk_r3,walk_r4}, 0.25f);
+        up = new Animation(new Bitmap[]{up1, up2, up3, up4},0.25f);
+
+        anim = new AnimationManager(new Animation[]{idle, walk_right, walk_left, up});
 
     }
 
@@ -52,9 +57,6 @@ public class Player implements GameObject{
 
     @Override
     public void draw(Canvas c) {
-        //Paint p = new Paint();
-        //p.setColor(color);
-        //c.drawRect(rectangle, p);
         anim.draw(c, rectangle);
     }
 
@@ -64,14 +66,17 @@ public class Player implements GameObject{
     }
 
     public void update(Point p){
-        float oldleft = rectangle.left;
+        float oldLeft = rectangle.left;
         rectangle.set(p.x - rectangle.width()/2, p.y - rectangle.height()/2, p.x + rectangle.width()/2, p.y + rectangle.height()/2);
 
         int state = 0;
-        if (rectangle.left - oldleft > 5)
+
+        if (rectangle.left - oldLeft > 0)
             state = 1;
-        else if(rectangle.left - oldleft < -5)
+        else if(rectangle.left - oldLeft < -10)
             state = 2;
+        else if (rectangle.left == oldLeft)
+            state = 3;
 
         anim.playAnimation(state);
         anim.update();
